3071260f04ad84a427e3e78a4607beb27b1029dc
luzia <luzia.castro29@gmail.com>
2024-02-23 21:38:00 -0400

    Upload image
    Signed-off-by: luzia <luzia.castro29@gmail.com>

--- src/web/home.py
+++ src/web/home.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from PIL import Image
-from src.view.getfile import Getimg
 
 
 uploaded_files = st.file_uploader("Upload your file here...", accept_multiple_files=True)
@@ -11,8 +10,6 @@ try:
         st.image(imagem)
         st.button("Gerar Jogo")
         imagem.save("/home/luzia-tpv/Documentos/CV-AI/CV-SpotTheDifference/src/img/teste.png")
-        img = Getimg.get_file()
-        print(img)
 except NameError:
   print("Variable x is not defined")
 
